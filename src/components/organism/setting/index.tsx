638b1acc386835cbafe52f36f87601e4ee14cbc5
Hiep Nguyen Van <hiepnv.wt@gmail.com>
2021-06-27 17:25:26 +0700

    feat: using reactjs-popup

--- src/components/organism/setting/index.tsx
+++ src/components/organism/setting/index.tsx
@@ -1,26 +1,24 @@
-import React, { forwardRef, useRef } from 'react';
-import { useOnClickOutside } from 'components/hooks';
-import { IPopupProps } from 'types/popup';
+import Popup from 'reactjs-popup';
+import { AiOutlineClose } from 'react-icons/ai';
+import Button from 'components/atom/button';
 import './style.scss';
 
-export type Ref = HTMLDivElement;
+type Props = {
+  openSetting: boolean;
+  closeModal: any;
+};
 
-const Setting = forwardRef<Ref, IPopupProps>(({ open, children }, ref) => {
-  const contentRef = useRef<HTMLElement>(null);
-  const triggerRef = useRef<HTMLElement>(null);
-
-  const trigger = triggerRef.current?.getBoundingClientRect();
-
-  useOnClickOutside(
-    trigger ? [contentRef, triggerRef] : [contentRef],
-    () => {
-      console.log('Click outside');
-    },
-    open,
-  );
-  const isOpen = true;
-
-  return isOpen && <div ref={ref}>{children}</div>;
-});
+const Setting = ({ openSetting, closeModal }: Props): JSX.Element => (
+  <Popup open={openSetting} onClose={closeModal}>
+    <div className="setting-content">
+      <div className="setting--header">
+        <div className="setting--heading">setting</div>
+        <Button className="setting--button__close" onClick={closeModal}>
+          <AiOutlineClose />
+        </Button>
+      </div>
+    </div>
+  </Popup>
+);
 
 export default Setting;
--- src/components/pages/layout.tsx
+++ src/components/pages/layout.tsx
@@ -2,25 +2,17 @@ import Header from 'components/organism/header';
 import Timer from 'components/organism/timer';
 import Tasks from 'components/organism/tasks';
 import Footer from 'components/organism/footer';
-import Setting from 'components/organism/setting';
-import React from 'react';
 
 // TODO: Layout for UI - Not neccessary in this app - has one route
-const Layout = (): JSX.Element => {
-  const ref = React.createRef<HTMLDivElement>();
-  return (
-    <div className="wrapper">
-      <main>
-        <Header />
-        <Setting open ref={ref}>
-          Click me!
-        </Setting>
-        <Timer pomodoroTime={5000} endTimePlan={Date.now() + 5000} />
-        <Tasks />
-      </main>
-      <Footer />
-    </div>
-  );
-};
+const Layout = (): JSX.Element => (
+  <div className="wrapper">
+    <main>
+      <Header />
+      <Timer pomodoroTime={5000} endTimePlan={Date.now() + 5000} />
+      <Tasks />
+    </main>
+    <Footer />
+  </div>
+);
 
 export default Layout;
