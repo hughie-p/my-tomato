a85a031524f39c3e03e73933adbfff021a4bd7dc
Hiep Nguyen Van <hiepnv.wt@gmail.com>
2021-07-07 06:23:27 +0700

    feat: login with facebook

--- src/components/organisms/Login/index.tsx
+++ src/components/organisms/Login/index.tsx
@@ -1,8 +1,8 @@
 import { FaFacebookSquare, FaUserCircle } from 'react-icons/fa';
 import Popup from 'reactjs-popup';
+import { AiOutlineGoogle } from 'react-icons/ai';
 import { getFirebaseAuth } from 'contexts/auth';
 import './style.scss';
-import { AiOutlineGoogle } from 'react-icons/ai';
 
 const LoginButton = () => (
   <button type="button">
@@ -20,7 +20,18 @@ const LoginModal = (): JSX.Element => {
     }
   };
 
-  const handleLoginFacebook = () => {};
+  const handleLoginFacebook = () => {
+    try {
+      getFirebaseAuth()
+        .then(({ auth, facebookProvider }) => auth.signInWithPopup(facebookProvider))
+        .catch((err) => {
+          alert(err);
+          return;
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <Popup
